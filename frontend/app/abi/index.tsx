0e8bc63a3e1a1978c1e084eca205a1eab1168230
Abidoyesimze <similoluwaeyitayoabidoye@gmail.com>
2025-12-08 13:13:52 +0100

    feat: add contract ABIs and configuration with deployed addresses

---
+++ frontend/app/abi/index.tsx
@@ -0,0 +1,12 @@
+import NGORegistry from "./NGORegistry.json";
+import DonationRouter from "./DonationRouter.json";
+
+export const NGORegistryContract = {
+    abi: NGORegistry,
+    address: "0xA49d0Fb2966B8D54Bd7Ec182dC93979763A58128"
+}
+
+export const DonationRouterContract = {
+    abi: DonationRouter,
+    address: "0x0a563f6Ca5fDa6410165E75f7b50b5b942573e87"
+}
